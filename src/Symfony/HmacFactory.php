5fbf077aba11841ef2a80213aadc387ae90e62ce
Miro Michalicka <miroslav.michalicka@cheppers.com>
2017-10-27 13:58:06 +0200

    Fix coding standards.

--- src/Symfony/HmacFactory.php
+++ src/Symfony/HmacFactory.php
@@ -8,25 +8,30 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 
-class HmacFactory implements SecurityFactoryInterface {
-  public function create(ContainerBuilder $container, $id, $config, $userProvider, $defaultEntryPoint) {
-    $providerId = 'security.authentication.provider.hmac.' . $id;
-    $container->setDefinition($providerId, new ChildDefinition('hmac.security.authentication.provider'));
+class HmacFactory implements SecurityFactoryInterface
+{
+    public function create(ContainerBuilder $container, $id, $config, $userProvider, $defaultEntryPoint)
+    {
+        $providerId = 'security.authentication.provider.hmac.' . $id;
+        $container->setDefinition($providerId, new ChildDefinition('hmac.security.authentication.provider'));
 
-    $listenerId = 'security.authentication.listener.hmac.' . $id;
-    $container->setDefinition($listenerId, new ChildDefinition('hmac.security.authentication.listener'));
+        $listenerId = 'security.authentication.listener.hmac.' . $id;
+        $container->setDefinition($listenerId, new ChildDefinition('hmac.security.authentication.listener'));
 
-    return array($providerId, $listenerId, $defaultEntryPoint);
-  }
+        return array($providerId, $listenerId, $defaultEntryPoint);
+    }
 
-  public function getPosition() {
-    return 'pre_auth';
-  }
+    public function getPosition()
+    {
+        return 'pre_auth';
+    }
 
-  public function getKey() {
-    return 'hmac_auth';
-  }
+    public function getKey()
+    {
+        return 'hmac_auth';
+    }
 
-  public function addConfiguration(NodeDefinition $node) {
-  }
+    public function addConfiguration(NodeDefinition $node)
+    {
+    }
 }
