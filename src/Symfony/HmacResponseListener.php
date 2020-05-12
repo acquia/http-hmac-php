8751c8b837efa18771b0887066cce5b37f12f7b9
Jakob Perry <japerry007@gmail.com>
2020-05-12 01:00:39 -0700

    LCH-4065: Update diactoros to support Drupal 8.6+.

--- src/Symfony/HmacResponseListener.php
+++ src/Symfony/HmacResponseListener.php
@@ -3,7 +3,12 @@
 namespace Acquia\Hmac\Symfony;
 
 use Acquia\Hmac\ResponseSigner;
+use Laminas\Diactoros\ResponseFactory;
+use Laminas\Diactoros\ServerRequestFactory;
+use Laminas\Diactoros\StreamFactory;
+use Laminas\Diactoros\UploadedFileFactory;
 use Symfony\Bridge\PsrHttpMessage\Factory\DiactorosFactory;
+use Symfony\Bridge\PsrHttpMessage\Factory\PsrHttpFactory;
 use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -27,11 +32,18 @@ class HmacResponseListener implements EventSubscriberInterface
         $response = $event->getResponse();
 
         if ($request->attributes->has('hmac.key')) {
-            $psr7Factory = new DiactorosFactory();
+            if (class_exists(PsrHttpFactory::class))
+            {
+                $httpMessageFactory = new PsrHttpFactory(new ServerRequestFactory(), new StreamFactory(), new UploadedFileFactory(), new ResponseFactory());
+            }
+            else
+            {
+                $httpMessageFactory = new DiactorosFactory();
+            }
             $foundationFactory = new HttpFoundationFactory();
 
-            $psr7Request = $psr7Factory->createRequest($request);
-            $psr7Response = $psr7Factory->createResponse($response);
+            $psr7Request = $httpMessageFactory->createRequest($request);
+            $psr7Response = $httpMessageFactory->createResponse($response);
 
             $signer = new ResponseSigner($request->attributes->get('hmac.key'), $psr7Request);
             $signedResponse = $signer->signResponse($psr7Response);
